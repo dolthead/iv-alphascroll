bd87d3dc43e74f1479081612ac4237b6c466e819
Todd H <github@simplybest.com>
2021-12-08 21:26:48 -0700

    add setup script version of main page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue'
+import Home2 from '../views/Home2.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +12,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/setup',
+    name: 'Home2',
+    component: Home2
   }
 ]
 
---
+++ src/views/Home2.vue
@@ -0,0 +1,89 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>{{ pageTitle }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :scrollY="!scrolling">
+      <ion-toolbar>
+        <div class="search">
+          <ion-input placeholder="Search">
+            <ion-icon :icon="search"></ion-icon>
+          </ion-input>
+        </div>
+      </ion-toolbar>
+
+      <ion-item-group v-for="group of groupedData" :key="group.key">
+        <ion-item-divider>
+          <ion-label :data-group="group.key">
+            {{ group.key }}
+          </ion-label>
+        </ion-item-divider>
+        <ion-item v-for="user of group.users" :key="user.uid" lines="none">
+          <ion-label>
+            {{ user.firstName }} <b>{{ user.lastName }}</b>
+          </ion-label>
+        </ion-item>
+      </ion-item-group>
+
+      <alphabet-scroll @letter-selected="goToLetter" @scrolling-letter="setScrolling" @touchmove.prevent></alphabet-scroll>
+    </ion-content>
+
+  </ion-page>
+</template>
+
+<script lang="ts" setup>
+
+  import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonInput, IonItemGroup, IonItem, IonItemDivider, IonLabel } from '@ionic/vue';
+  import data from '../data.json';
+  import { search } from 'ionicons/icons';
+  import AlphabetScroll from '../components/alphabet-scroll.vue';
+
+  const pageTitle = 'Ionic Vue setup script';
+  const groupedData: any[] = [];
+  let scrolling = false;
+
+  const sorted = data.sort((a,b) => {
+    if (a.last_name + a.first_name < b.last_name + b.first_name) { return -1; }
+    if (a.last_name + a.first_name > b.last_name + b.first_name) { return 1; }
+    return 0;
+  });
+
+  let last = '';
+  sorted.forEach(({ first_name: firstName, last_name: lastName, uid }) => {
+    if (lastName[0] !== last) {
+      last = lastName[0];
+      groupedData.push({ key: last, users: [] });
+    }
+    groupedData[groupedData.length - 1].users.push({ firstName, lastName, uid });
+  });
+
+  const goToLetter = (letter: string) => {
+    const dataGroup = document.querySelector(`[data-group="${ letter }"]`);
+    if (dataGroup) {
+      dataGroup.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const setScrolling = (ev: boolean) => { scrolling = ev; };
+
+</script>
+
+<style lang="scss" scoped>
+.search {
+  display: flex;
+
+  ion-input {
+    margin: 10px;
+    --padding-start: 10px;
+    --background: #e7e6e8;
+    border-radius: 8px;;
+  }
+
+  ion-icon {
+    margin-left: 10px;
+  }
+}
+</style>
